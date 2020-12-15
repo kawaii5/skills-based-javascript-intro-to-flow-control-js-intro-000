c28cbdf4a77b6cb1203d7bbe7bfcea5c203ae388
Michelle Wang <oc3anazns3a@gmail.com>
2020-12-15 02:04:55 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -12,7 +12,9 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-  if(age >= 13 && age <= 19) {
+  if(age <= 12) {
+    return "You are a kid"
+  } else if(age >= 13 && age <= 19) {
     return "You are a teenager!"
   }
 }
