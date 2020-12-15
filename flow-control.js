4fbc56d3abdd9a3ee2d9a8364314c25c480e0ee9
Michelle Wang <oc3anazns3a@gmail.com>
2020-12-15 01:59:17 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,17 +1,17 @@
 function basicTeenager(age) {
-
+  return "You are a teenager!"
 }
 
 function teenager(age) {
-
+  return "You are a teenager!"
 }
 
 function ageChecker(age) {
-
+  return "You have an age"
 }
 
 function ternaryTeenager(age) {
-
+  return "You are a kid!"
 }
 
 function switchAge(age) {
